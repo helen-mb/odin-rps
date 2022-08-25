c79ea6b98052bdd134d947f214fac7daa2630512
Helen Burger <burger.helen.m@gmail.com>
2022-08-24 17:16:11 -0700

    Condense instructions into a rough outline

--- script.js
+++ script.js
@@ -10,11 +10,33 @@
 
 // Make your function’s playerSelection parameter case-insensitive 
 // (so users can input rock, ROCK, RocK or any other variation).
-
 // Important note: you want to return the results of this function call, not console.log() them. 
 // You’re going to use what you return later on, 
-// so let’s test this function by using console.log to see the results.
+// but let’s test this function by using console.log to see the results.
 
 // Write a NEW function called game(). 
 // Call the playRound function inside of this one to play a 5 round game 
 // that keeps score and reports a winner or loser at the end.
+
+// At this point you should be using console.log() to display 
+// the results of each round and the winner at the end.
+// Use prompt() to get input from the user. 
+// --
+
+
+
+// A function for computer's play: 'getComputerChoice()'
+//    Returns either 'Rock' 'Paper' or 'Scissors'
+//    (*Probably involves random number generation; 1, 2, or 3?)
+
+// A function that plays a single round: 'playRound()' 
+//    Parameters: 'playerSelection', 'computerSelection'
+//    Gets player's selection through prompt().
+//    Makes player's selection case IN-sensitive.
+//    and declares a winner by returning a string.
+
+// A function that plays five rounds: 'game()'
+//    (*Loops may be a good choice)
+//    and displays results of each round in the console
+//    and keeps score to declare an ultimate winner or loser.
+
