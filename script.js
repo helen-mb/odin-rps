79be192575ff1b6184ec0ce78d8def91f1687d8f
Helen Burger <burger.helen.m@gmail.com>
2022-09-23 13:56:08 -0700

    Declare round outcomes and scores

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// A function for the computer's play
+// A function for the computer's action
 let computerSelection;
 function getComputerSelection() {
     computerSelection = Math.floor(Math.random() * 3) + 1;
@@ -15,7 +15,7 @@ function getComputerSelection() {
     return computerSelection;
 }
 
-// For the player's play:
+// For the player's action:
 // Allowing the buttons to initiate playRound(), capture the player's selection, and pass the playRound() arguments
 const playButtons = document.getElementsByClassName('playButton');
 let playerSelection;
@@ -24,21 +24,22 @@ for (i=0; i<playButtons.length; i++) {
     playButtons.item(i).addEventListener('click', event => {
         playerSelection = event.target.id;
         playRound(playerSelection, getComputerSelection());
+        updateScore();
     })
 }
 
-// A function that plays a single round 
+const gameBoard = document.getElementById('gameBoard');
+// A function that plays a single round and returns the round outcome
 let outcome;
 function playRound(playerSelection, computerSelection) {
-    // Creating a div inside the gameBoard for displaying the players' choices:
-    const gameBoard = document.getElementById('gameBoard');
+    // Creating a div for displaying the players' choices inside the gameBoard:
+//xxx This creates a new div every round whereas I only need one div... maybe a "start game" function can do it?
     const choiceDisplay = document.createElement('div'); 
-
-    // Displaying the competing choices in the choiceDisplay div:
-    const competingChoices = document.createTextNode(`You chose "${playerSelection}" -- The computer chose "${computerSelection}"!`);
+    // Displaying the competing choices as a new paragraph in the choiceDisplay div:
+    const competingChoices = document.createElement('p');
+    const choicesText = document.createTextNode(`You chose "${playerSelection}" -- The computer chose "${computerSelection}"!`);
+    competingChoices.appendChild(choicesText);
     choiceDisplay.appendChild(competingChoices);
-    
-    // Adding the choiceDisplay div to the gameBoard:
     gameBoard.appendChild(choiceDisplay);
 
     // Using a switch statement to concatenate the opposing selections into winning or losing combinations
@@ -69,41 +70,40 @@ function playRound(playerSelection, computerSelection) {
         default:
             outcome = 'Sorry, what was that? :?';
     }
+    const outcomeReport = document.createElement('p');
+    const outcomeText= document.createTextNode(outcome);
+    outcomeReport.appendChild(outcomeText);
+    gameBoard.appendChild(outcomeReport);
     // Returning the round outcome so the game() function can keep score, below
     return outcome;
 }
 
-// A function that plays five rounds and keeps score
-function game() {
-    let round;
-    let computerScore = 0;
-    let playerScore = 0;
-
-    // Using a while loop to play the best of five rounds of playRound()
-    let i = 1
-    while (i > 0) { // TEMPORARILY REMOVED LOGIC TO PLAY ONLY 5 ROUNDS
-        round = i;
-        playRound();
-
-        // Changing the scores depending on the outcome of each round
-        // A round does not count if it was a draw or the player input an invalid word
-        
-        if (outcome.includes('Yay!')) {
-            computerScore += 0;
-            playerScore += 1;
-            i++;
-        } else if (outcome.includes('Oh no!')) {
-            computerScore += 1;
-            playerScore += 0;
-            i++;
-        } 
-        
-        // Declaring the outcome and score of each round
-        console.log(`The scores are: Computer ${computerScore}, Player ${playerScore}!`);
-        alert(outcome);
+// A function that keeps score for up to 5 rounds
+let computerScore = 0;
+let playerScore = 0;
+function updateScore() {
+    // Changing the scores depending on the outcome of each round
+    // A round does not count if it was a draw or the player input an invalid word
+    if (outcome.includes('Yay!')) {
+        computerScore += 0;
+        playerScore += 1;
+    } else if (outcome.includes('Oh no!')) {
+        computerScore += 1;
+        playerScore += 0;
     }
+        
+    // Declaring the outcome and score of each round
+//xxx This creates a new div every round whereas I only need one div... maybe a "start game" function can do it?
+    const scoreBoard = document.createElement('div'); 
+    // Displaying the updated scores as a new paragraph in the choiceDisplay div:
+    const scores = document.createElement('p');
+    const scoreValues = document.createTextNode(`The scores are: Computer ${computerScore}, Player ${playerScore}!`);
+    scores.appendChild(scoreValues);
+    scoreBoard.appendChild(scores);
+    gameBoard.appendChild(scoreBoard);
+    
 
-    // Declaring an ultimate winner
+    /* // Declaring an ultimate winner
     let winner;
     if (computerScore < playerScore) { 
         winner = `And the winner is... You, the player! With a final score of ${playerScore} vs. ${computerScore}.`
@@ -113,7 +113,7 @@ function game() {
         winner = `It\'s a tie! ${computerScore} vs. ${playerScore}. How did that happen...?`
     }
     console.log(winner);
-    alert(winner);
+    alert(winner); */
 }
 
 //game();
